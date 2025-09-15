12368a3a1bfc85c577942cf85096e7c0cec39359
eduardoerthal <eduardoerthaloliveira@gmail.com>
2025-09-14 23:27:34 -0300

    Criação da entidade Tarefa

---
+++ app/src/main/java/br/equipe07/ui/Tarefa.java
@@ -0,0 +1,18 @@
+package br.equipe07.ui;
+
+import java.io.Serializable;
+
+public class Tarefa implements Serializable {
+    private long id;
+    private String titulo;
+    private String descricao;
+
+    public Tarefa() {
+    }
+
+    public Tarefa(long id, String titulo, String descricao) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
+}
